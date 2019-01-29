96b1dcd943628b1b4829f1f449e023ee8a8ec42d
Sergey Morozov <sergmoro1@ya.ru>
2019-01-29 16:06:43 +0300

    constants definitions in migrations and make protected as a private in temp/Comment

--- src/temp/common/models/Comment.php
+++ src/temp/common/models/Comment.php
@@ -10,11 +10,15 @@ use sergmoro1\blog\models\BaseComment;
 
 class Comment extends BaseComment
 {
-	protected $commentFor = [1 => 'post', 2 => 'fund'];
+	private static $commentFor = [1 => 'post', 2 => 'fund'];
 
 	public function getFund()
 	{
 		return $this->hasOne(Fund::className(), ['id' => 'parent_id']);
 	}
+
+    public static function parentModelName($model) {
+        return self::$commentFor[$model];
+    }
 }
         
